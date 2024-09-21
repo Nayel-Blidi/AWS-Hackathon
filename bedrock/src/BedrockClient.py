8d03a83e7015e74a5908bf062d9999bb7aadb73c
Nayel-Blidi <nayel.blidi@capgemini.com>
2024-09-21 16:00:23 +0200

    Add: bedrock management class

---
+++ bedrock/src/BedrockClient.py
@@ -0,0 +1,68 @@
+import boto3
+import json
+import os, sys
+
+
+BEDROCK_DIR_PATH = os.path.dirname(os.path.dirname(__file__))
+if __name__ == "__main__":
+    sys.path.append(os.path.dirname(BEDROCK_DIR_PATH))
+
+from constants import AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_SESSION_TOKEN
+
+
+class BedrockClient():
+    """
+    Standard S3 class with all purpose methods.
+    """
+
+    def __init__(self, 
+                 aws_access_key_id:str = AWS_ACCESS_KEY,
+                 aws_secret_access_key:str = AWS_SECRET_KEY,
+                 aws_session_token: str = AWS_SESSION_TOKEN,
+                 ) -> None:
+        """
+        Initiates a connection to a given Bedrock model.
+
+        Args
+        ----
+        - aws_access_key_id: the user's access key id (private)
+        - aws_secret_access_key: the user's access key (private)
+
+        Note
+        ----
+        - `key` kwarg refers to a cloud path
+        - `path` kwarg refers to a local path
+        """
+
+        self.client = boto3.client(service_name='bedrock', 
+                                   aws_access_key_id=aws_access_key_id, 
+                                   aws_secret_access_key=aws_secret_access_key, 
+                                   aws_session_token=aws_session_token,
+                                   region_name='us-east-1')
+
+    def list_foundation_models(self):
+        """
+        List the available Amazon Bedrock foundation models.
+
+        :return: The list of available bedrock foundation models.
+        """
+
+        response = self.client.list_foundation_models()
+        models = response["modelSummaries"]
+        print(models)
+
+        return models
+    
+
+
+
+
+
+if __name__ == "__main__":
+    model_id = 'amazon.titan-text-lite-v1' 
+    prompt_text = 'What is the future of AI in aerospace?'
+    prompt_text = 'Give me a recipee of fish and chips?'
+    
+    bedrock_cli = BedrockClient()
+    bedrock_cli.list_foundation_models()
+    
